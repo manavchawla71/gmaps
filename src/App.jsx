f050b70005f8e118a1f804daab6c56c4d3303478
Manav Chawla <chawlamanav428@gmail.com>
2024-09-04 18:37:01 +0530

    geolocation access enabled

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,38 @@ import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "./App.css";
 
 function App() {
+  const [position, setposition] = useState(null);
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          setposition([pos.coords.latitude, pos.coords.longitude]);
+        },
+        (err) => {
+          setError(err.message);
+        }
+      );
+    } else {
+      setError("Geolocation is not supported by this browser.");
+    }
+  }, []);
   return (
     <>
       <MapContainer
         center={[28.892549, 76.5954186]}
-        zoom={13}
-        scrollWheelZoom={false}
+        zoom={9}
+        scrollWheelZoom={true}
         id="map"
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[28.892549, 76.5954186]}>
-          <Popup>Your Current location.</Popup>
-        </Marker>
+        {position && (
+          <Marker position={position}>
+            <Popup>You are here.</Popup>
+          </Marker>
+        )}
       </MapContainer>
     </>
   );
